5c296f10b67e0b7ffc39697f0594a1f53e84e7a4
Azra Javed <azrajaved2004@gmail.com>
2026-07-01 19:56:20 +0530

    solution

---
+++ bunary_tree_path.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<string> ans;
+
+    void dfs(TreeNode* root, string path) {
+        if (!root) return;
+
+        if (!path.empty())
+            path += "->";
+
+        path += to_string(root->val);
+
+        // Leaf node
+        if (!root->left && !root->right) {
+            ans.push_back(path);
+            return;
+        }
+
+        dfs(root->left, path);
+        dfs(root->right, path);
+    }
+
+    vector<string> binaryTreePaths(TreeNode* root) {
+        dfs(root, "");
+        return ans;
+    }
+};
