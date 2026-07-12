da2a181e2d47687cc56862083d2aa03293544492
Azra Javed <azrajaved2004@gmail.com>
2026-07-12 18:53:46 +0530

    solution

---
+++  Rank Transform of an Array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> arrayRankTransform(vector<int>& arr) {
+        vector<int> sorted = arr;
+        sort(sorted.begin(), sorted.end());
+
+        map<int, int> rank;
+        int currentRank = 1;
+
+        for (int num : sorted) {
+            if (rank.find(num) == rank.end()) {
+                rank[num] = currentRank;
+                currentRank++;
+            }
+        }
+
+        for (int i = 0; i < arr.size(); i++) {
+            arr[i] = rank[arr[i]];
+        }
+
+        return arr;
+    }
+};
